a16e1d828a23f7e037f42a2647b35ee15d10b27e
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-07-11 18:09:35 -0400

    feat: serve up exact comic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SlackBot = require("slackbots");
 const axios = require("axios");
 
 const bot = new SlackBot({
-  token: "<insert your slack bot token here>",
+  token: "<insert slack token here>",
   name: "xkcdbot"
 });
 
@@ -12,7 +12,11 @@ bot.on("start", () => {
     icon_emoji: ":robot_face:"
   };
 
-  bot.postMessageToChannel("general", "Get your XKCD on with @xkcdbot!", params);
+  const helpMessage = `This bot accepts the following commands:\
+  '@xkcdbot random', '@xkcdbot get <integer value>', \
+  '@xkcdbot help'`;
+
+  bot.postMessageToChannel("general", `I serve up XKCD comics! ${helpMessage}`, params);
 });
 
 // error handler
@@ -28,14 +32,29 @@ bot.on("message", data => {
 });
 
 function handleIncomingMessage(message) {
-  console.log('message is: ', message);
-  if (message.includes(" random") && message !== 'undefined') {
+  let arrMessageText = message && message.split(" ");
+
+  if (arrMessageText && arrMessageText.includes("random")) {
     randomXKCD();
+  } else if (arrMessageText && arrMessageText.includes("get")) {
+    let number = arrMessageText[2];
+    getXKCD(+number);
+  } else if (arrMessageText && arrMessageText.includes("help")) {
+    const helpMesssage = `This bot accepts the following commands
+      '@xkcdbot random' '@xkcdbot get <integer value>'
+      '@xkcdbot help'`;
+
+    const params = {
+      icon_emoji: ":confused:"
+    };
+
+    bot.postMessageToChannel("general", helpMesssage, params);
   }
 }
 
 function randomXKCD() {
-  let randomNum = Math.floor(Math.random() * 2000) + 1;
+  let upperLimit = 2018;
+  let randomNum = Math.floor(Math.random() * upperLimit) + 1;
 
   axios
     .get(`http://xkcd.com/${randomNum}/info.0.json`)
@@ -44,10 +63,42 @@ function randomXKCD() {
       const title = res.data.title;
 
       const params = {
-        icon_emoji: ":joy:"
+        icon_emoji: ":thinking_face:"
       };
 
       bot.postMessageToChannel("general", `${title}: ${img}`, params);
     })
     .catch(err => console.log(err));
+}
+
+function getXKCD(number) {
+  if (typeof number !== 'number') return;
+  let lowerLimit = 1;
+  let upperLimit = 2018;
+  let outOfRangeMessage = `Comic number out of range!\
+  Passed in integer must be greater-than ${lowerLimit} and less-than ${upperLimit}.`;
+
+  const params = {
+    icon_emoji: ":bangbang:"
+  };
+
+  if (number < lowerLimit) {
+    bot.postMessageToChannel("general", outOfRangeMessage, params);
+  } else if (number > upperLimit) {
+    bot.postMessageToChannel("general", outOfRangeMessage, params);
+  } else {
+    axios
+      .get(`http://xkcd.com/${number}/info.0.json`)
+      .then(res => {
+        const img = res.data.img;
+        const title = res.data.title;
+
+        const params = {
+          icon_emoji: ":thinking_face:"
+        };
+
+        bot.postMessageToChannel("general", `${title}: ${img}`, params);
+      })
+      .catch(err => console.log(err));
+  }
 }
